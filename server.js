72035bcef8cb84ac03e07fe4f14e89a781a3015d
João Guilherme Couto <168004754+JnGuilherme07@users.noreply.github.com>
2025-11-24 16:36:10 -0300

    alterações

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js (ESM)
+
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -9,20 +9,19 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-// pasta pública (serve CSS, JS, imagens, etc)
 app.use(express.static(__dirname));
 
-// rota da página inicial
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// rota da página de projeto
+
 app.get("/projeto", (req, res) => {
   res.sendFile(path.join(__dirname, "projeto.html"));
 });
 
-// rota 404
+
 app.use((req, res) => {
   res.status(404).send("Página não encontrada");
 });
